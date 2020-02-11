f01a3ce1b71d6c0547bd4edefee7962e56832e64
Manpreet Sandhu <m_sandhu@live.ca>
2020-02-11 09:47:18 -0700

    added speed encoder

--- bot_control.py
+++ bot_control.py
@@ -1,6 +1,11 @@
 import gpiozero as gpio
+import math
 import proximity
 import gyro
+
+# motor wheel constants
+WHEEL_DIAMETER = 63.9  # diameter of the wheel in millimeters
+STEP_COUNT = 20.0  # number of slots in the wheel speed encoder ring
 
 # Motor gpio setup (forward pin, backward pin)
 right_motors = gpio.Motor(17, 27)
@@ -9,6 +14,28 @@ left_motors = gpio.Motor(24, 23)
 # speed encoder sensor setup
 right_speed = gpio.DigitalInputDevice(25)
 left_speed = gpio.DigitalInputDevice(22)
+right_speed.when_activated = ev_right_count  #event
+left_speed.when_activated = ev_left_count #event
+
+# pulse counters for speed sensors
+counter_right = 0
+counter_left = 0
+
+#right speed pulse counter event
+def ev_right_count():
+    counter_right += 1
+
+#left speed pulse counter event
+def ev_left_count():
+    counter_left += 1
+
+#converts centimeters to the number of steps required by
+#the wheel to cover the distance provided in centimeters
+#1 step = 1 slot on the encoder wheel
+def cm_to_steps(cm):
+    circumference = (WHEEL_DIAMETER * math.pi) / 10 # divide by 10 to go from mm to cm
+    steps = (circumference / STEP_COUNT) * cm #steps required to travel the given cm distance
+    return int(steps)  # return the number of steps as integer
 
 
 # move forward with a speed between 0-1 (default = 1)
