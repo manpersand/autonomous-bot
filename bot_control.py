f7c563ba46f89793b26225742ff7f023731e5fc5
Manpreet Sandhu <m_sandhu@live.ca>
2020-02-10 22:50:05 -0700

    updated run

--- bot_control.py
+++ bot_control.py
@@ -2,38 +2,40 @@ import gpiozero as gpio
 import proximity
 import gyro
 
-#Motor gpio setup (forward pin, backward pin)
-right_motors = gpio.Motor(17,27)
-left_motors = gpio.Motor(24,23)
+# Motor gpio setup (forward pin, backward pin)
+right_motors = gpio.Motor(17, 27)
+left_motors = gpio.Motor(24, 23)
 
-#speed encoder sensor setup 
+# speed encoder sensor setup
 right_speed = gpio.DigitalInputDevice(25)
 left_speed = gpio.DigitalInputDevice(22)
 
-#move forward with a speed between 0-1 (default = 1)
-def forward(speed = 1):
+
+# move forward with a speed between 0-1 (default = 1)
+def forward(speed=1):
     left_motors.forward(speed)
     right_motors.forward(speed)
-    
-#move backward with a speed between 0-1 (default = 1)
-def backward(speed = 1):
+
+
+# move backward with a speed between 0-1 (default = 1)
+def backward(speed=1):
     left_motors.backward(speed)
     right_motors.backward(speed)
 
-#stop all motors
+
+# stop all motors
 def stop():
     left_motors.stop()
     right_motors.stop()
 
-#skid steer LEFT with a speed between 0-1 (default = 1)
-def left(speed = 1):
+
+# skid steer LEFT with a speed between 0-1 (default = 1)
+def left(speed=1):
     left_motors.backward(speed)
     right_motors.forward(speed)
-    
-#skid steer RIGHT with a speed between 0-1 (default = 1)
-def right(speed = 1):
+
+
+# skid steer RIGHT with a speed between 0-1 (default = 1)
+def right(speed=1):
     left_motors.forward(speed)
     right_motors.backward(speed)
-
-
-
--- run.py
+++ run.py
@@ -1,48 +1,43 @@
 from time import sleep
-import bot_control
+import bot_control as bot
 import curses
-
-
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
 screen = curses.initscr()
-curses.noecho() 
+curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
 try:
-        while True:
-            Stop()
-            char = screen.getch()
-            if char == ord('q'):
-                break
-            elif char == curses.KEY_UP:
-                Forward(0.7)
-                print
-            elif char == curses.KEY_DOWN:
-                Backward(0.7)
-            elif char == curses.KEY_RIGHT:
-                TurnRight(0.7)
-            elif char == curses.KEY_LEFT:
-                TurnLeft(0.7)
-            elif char == ord('d'):
-                Forward(0.7)
-                sleep(0.5)
-                Backward(0.7)
-                sleep(0.5)
-                TurnLeft(0.7)
-                sleep(0.5)
-                TurnRight(0.70)
-                sleep(0.5)
-                Stop()
+    while True:
+        bot.stop()
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_UP:
+            bot.forward(0.7)
+        elif char == curses.KEY_DOWN:
+            bot.backward(0.7)
+        elif char == curses.KEY_RIGHT:
+            bot.right(0.7)
+        elif char == curses.KEY_LEFT:
+            bot.left(0.7)
+        elif char == ord('d'):
+            bot.forward(0.7)
+            sleep(0.5)
+            bot.backward(0.7)
+            sleep(0.5)
+            bot.left(0.7)
+            sleep(0.5)
+            bot.right(0.70)
+            sleep(0.5)
+            bot.Stop()
 
-##            if speedSensRight.value == True:
-##                print("ON"),
-##            else:
-##                print("OFF"),
-             
+
 finally:
-    #Close down curses properly, inc turn echo back on!
-    curses.nocbreak(); screen.keypad(0); curses.echo()
+    # Close down curses properly, inc turn echo back on!
+    curses.nocbreak();
+    screen.keypad(0);
+    curses.echo()
     curses.endwin()
