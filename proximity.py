bc4abacb6696e3e0f8a5cede337bf12a2325facf
Manpreet Sandhu <m_sandhu@live.ca>
2020-02-12 11:00:26 -0700

    added proximity

--- proximity.py
+++ proximity.py
@@ -1,2 +1,10 @@
-import board
 import time
+from gpiozero import DistanceSensor
+
+sensor = DistanceSensor(trigger=5, echo=6)
+
+while True:
+    print(f'Distance : {sensor.distance * 100} cm', end='\n')
+    time.sleep(1)
+
+
